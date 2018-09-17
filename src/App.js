c2b4c88df4f5fb679c53d816c527384868170e71
Eduardo Pilla Portilho <eduardo@portilho.com>
2018-09-17 16:01:50 +0200

    Querying GitHub repos

--- src/App.js
+++ src/App.js
@@ -1,11 +1,27 @@
 import React, { Component } from "react";
 import Repos from "./Repos";
-import logo from "./logo.svg";
 import "./App.css";
 
 class App extends Component {
+  state = {
+    query: ""
+  };
+
+  handleQueryChange = event => {
+    this.setState({ query: event.target.value });
+  };
+
   render() {
-    return <Repos />;
+    return (
+      <div>
+        <input
+          type="text"
+          value={this.state.query}
+          onChange={this.handleQueryChange}
+        />
+        <Repos queryString={this.state.query} />
+      </div>
+    );
   }
 }
 
--- src/Repos.js
+++ src/Repos.js
@@ -2,32 +2,52 @@ import React from "react";
 import { Query } from "react-apollo";
 import gql from "graphql-tag";
 
-const Repos = () => (
-  <Query
-    query={gql`
-      {
-        viewer {
-          name
-          repositories(last: 3) {
-            nodes {
-              name
+const SEARCH_REPOS_GQL = gql`
+  query SearchMostTop10Star($queryString: String!) {
+    search(query: $queryString, type: REPOSITORY, first: 10) {
+      repositoryCount
+      edges {
+        node {
+          ... on Repository {
+            name
+            description
+            descriptionHTML
+            owner {
+              login
             }
+            stargazers {
+              totalCount
+            }
+            forks {
+              totalCount
+            }
+            updatedAt
           }
         }
       }
-    `}
-  >
-    {({ loading, error, data }) => {
-      if (loading) return <p>Loading...</p>;
-      if (error) return <p>Error :(</p>;
+    }
+  }
+`;
 
-      return data.viewer.repositories.nodes.map(({ name }) => (
-        <div key={name}>
-          <p>{`${name}`}</p>
-        </div>
-      ));
-    }}
-  </Query>
-);
+const Repos = ({ queryString }) => {
+  if (!queryString || !queryString.trim().length) return null;
+  return (
+    <Query query={SEARCH_REPOS_GQL} variables={{ queryString }}>
+      {({ loading, error, data }) => {
+        if (loading) return <p>Loading...</p>;
+        if (error) {
+          console.log(error);
+          return <p>Error :(</p>;
+        }
+
+        return data.search.edges.map(({ node: { name } }) => (
+          <div key={name}>
+            <p>{`${name}`}</p>
+          </div>
+        ));
+      }}
+    </Query>
+  );
+};
 
 export default Repos;
